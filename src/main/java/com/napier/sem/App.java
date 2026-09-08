0240f3bda552d443249208b63f4e8507f3ba6e6c
paithanthtoo <Opvin2004@gmail.com>
2026-09-08 22:35:47 +0630

    Added Hello World

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,7 @@
 package com.napier.sem;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class App {
-    public static void main(String[] args) {
-        System.out.print("Hello World");
-        }
+    public static void main(String[] args){
+        System.out.println("Hello World");
     }
+}
